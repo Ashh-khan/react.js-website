c56ed455a1800726f2c6476cbb42e912ca4c8f59
ashh <ashhkhan123@gmail.com>
2024-01-04 19:24:17 +0530

    setup web app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+ import { HashRouter,Routes,Route} from 'react-router-dom'
+ import Navbar from './Navbar'
+ import Home from './components/Home'
+ import Contact from "./components/Contact"
+ import Service from './components/Service'
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import 'bootstrap/dist/js/bootstrap.bundle.min';
+import About from './components/About'
+import Footer from './components/Footer'
+
+
+
+
+function App() {
+  return (
+    <HashRouter>
+    <Navbar/>
+    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/Service' element={<Service/>}/>
+        <Route path='/About' element={<About/>}/>
+       
+      </Routes>
+      <Footer/>
+      
+     
+    </HashRouter>
+  )
+}
+
+export default App
